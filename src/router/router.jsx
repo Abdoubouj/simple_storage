cceb2da6e288a60122a13a4c860d5dea2defd3d3
AbdouBouj <abderrahimboujedar@gmail.com>
2024-03-01 21:21:02 +0100

    update

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,12 +3,14 @@ import Home from "../Home/Home";
 import Layout from "../Layout/Layout";
 import HowItWork from "../HowItWork/HowItWork";
 import Pricing from "../Pricing/Pricing";
+import Contact from "../Contact/Contact";
 
 export const router = createBrowserRouter([
     {path:"/",element:<Layout/>,children:[
         {path:"" , element:<Home/>},
         {path:"/HowItWork" , element:<HowItWork/>},
         {path:"/Pricing" , element:<Pricing/>},
+        {path:"/Contact" , element:<Contact/>},
         {path:"*",element:<h1 className="p-5 min-h-screen text-red-600 font-[700]">PAGE NOT FOUND</h1>}
     ]},
 ])
