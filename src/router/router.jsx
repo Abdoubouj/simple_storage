9abfbd2ba4f7a4a655e91d37c3f5bc974b5d8e53
AbdouBouj <abderrahimboujedar@gmail.com>
2024-03-01 20:36:38 +0100

    update

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../Home/Home";
 import Layout from "../Layout/Layout";
 import HowItWork from "../HowItWork/HowItWork";
+import Pricing from "../Pricing/Pricing";
 
 export const router = createBrowserRouter([
     {path:"/",element:<Layout/>,children:[
         {path:"" , element:<Home/>},
         {path:"/HowItWork" , element:<HowItWork/>},
+        {path:"/Pricing" , element:<Pricing/>},
         {path:"*",element:<h1 className="p-5 min-h-screen text-red-600 font-[700]">PAGE NOT FOUND</h1>}
     ]},
 ])
